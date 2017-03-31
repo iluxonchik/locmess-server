e2e4925e2bca290de49d192c381ca72bf51a4224
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-03-31 09:44:42 +0100

    test: add test for user managment
    User login, logout and user auth

--- tests/test_users.py
+++ tests/test_users.py
@@ -6,7 +6,6 @@ managment.
 # IMPORTANT: dirty hack to make the tests use a separate testing database.
 # Those two lines should be the first ones in every test file
 from locmess import settings
-setattr(settings, 'DB_FILE_NAME', settings.TEST_DB_FILE_NAME)
 
 import os
 import requests
@@ -14,10 +13,12 @@ import unittest
 from pony.orm import *
 
 from locmess.locmess import LocMess
-from locmess.exceptions import UserAlreadyExistsError
+from locmess.exceptions import (UserAlreadyExistsError, UserNotFoundError,
+                                TokenInvalidError)
 
 import locmess.tables as tables
-from locmess.tables import User, Token
+from locmess.tables import User, db
+
 
 class UserManagmentTestCase(unittest.TestCase):
 
@@ -26,9 +27,10 @@ class UserManagmentTestCase(unittest.TestCase):
 
     def setUp(self):
         self._lm = LocMess()
+        db.create_tables()
 
     def tearDown(self):
-        os.remove(settings.TEST_PATH + settings.TEST_DB_NAME)
+        db.drop_all_tables(with_all_data=True)
 
     @db_session
     def test_new_user_registration(self):
@@ -45,7 +47,7 @@ class UserManagmentTestCase(unittest.TestCase):
     def test_user_login_success(self):
         self._lm.add_user(self.U1_USR, self.U1_PWD)
         token = self._lm.login(self.U1_USR, self.U1_PWD)
-        self.assertIsNotNone(token, 'Returned token is None (login failed)')
+        self.assertIsNotNone(token, 'Returned token is None (login failed).')
 
     def test_user_login_failure(self):
         WRONG_PWD = 'if you ever knew dre, you would say I was The New Dre'
@@ -53,15 +55,61 @@ class UserManagmentTestCase(unittest.TestCase):
         token = self._lm.login(self.U1_USR, WRONG_PWD)
         self.assertIsNone(token, 'Returned token is not None (login success)')
 
-
+    @db_session
     def test_user_logout(self):
-        pass
+        """
+        Make sure that the token (i.e. the session id is destroyed on logout).
+        """
+        self._lm.add_user(self.U1_USR, self.U1_PWD)
+        user = User.get(username=self.U1_USR)
+        token = user.token
+        self.assertIsNone(token, 'Newly created user already has a token')
 
+        token = self._lm.login(self.U1_USR, self.U1_PWD)
+        user = User.get(username=self.U1_USR)
+        token = user.token
+        self.assertIsNotNone(token)
+
+        # now, let's logout
+        self._lm.logout(self.U1_USR, token)
+        user = User.get(username=self.U1_USR)
+        token = user.token
+        self.assertIsNone(token, 'Token not removed on logout.')
+
+    @db_session
     def test_new_token_created(self):
-        pass
+        self._lm.add_user(self.U1_USR, self.U1_PWD)
+        token = self._lm.login(self.U1_USR, self.U1_PWD)
+        user = self._lm.get_user(self.U1_USR)
+        saved_token = user.token
+        self.assertEqual(token, saved_token)
 
-    def test_user_auth_with_token(self):
-        pass
+        prev_token = token
+        token = self._lm.login(self.U1_USR, self.U1_PWD)
+        self.assertNotEqual(token, prev_token, 'Token was not re-generated'
+                                               'on login.')
 
-    def test_user_auth_fail_with_wrong_token(self):
-        pass
+        saved_token = User.get(username=self.U1_USR).token
+        self.assertEqual(token, saved_token, 'Token mismatch')
+
+    def test_user_logout_fail_with_wrong_token(self):
+        """
+        Try performing a logout action with a wrong token and make sure that
+        it's refused.
+        """
+        self._lm.add_user(self.U1_USR, self.U1_PWD)
+        token = self._lm.login(self.U1_USR, self.U1_PWD)
+
+        with self.assertRaises(TokenInvalidError):
+            self._lm.logout(self.U1_USR, b'the chronic 1992')
+
+    def test_user_logout_fail_with_non_exitent_user(self):
+        """
+        Try performing a logout action with non-existing user and make sure that
+        it's refused.
+        """
+        self._lm.add_user(self.U1_USR, self.U1_PWD)
+        token = self._lm.login(self.U1_USR, self.U1_PWD)
+
+        with self.assertRaises(UserNotFoundError):
+            self._lm.logout('Dr.Dre', token)
