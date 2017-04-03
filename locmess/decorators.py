ca48b1ec9c5e25b1c1ab927a149de227436465f6
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-04-03 09:20:21 +0100

    feat: update server to support: add/get location and messages (by GPS)

--- locmess/decorators.py
+++ locmess/decorators.py
@@ -1,7 +1,9 @@
+import json, sys, traceback
 from functools import wraps
 from pony.orm import *
 from locmess.tables import User
 from locmess.exceptions import UserNotFoundError, TokenInvalidError
+
 def authentication_required(f):
     """
     Makes sure that the user is correctly authenticated.
@@ -42,4 +44,32 @@ def authentication_required(f):
             raise TokenInvalidError('Token "{}" is invalid for user "{}".'.format(token, username))
 
         return f(*args, **kwargs)
+    return decorated_function
+
+def handle_expcetions(f):
+    """
+    Handles any excepitons and returns the error message, as well as a 401
+    response code back to the client. If everything goes well, headers are
+    set to the "OK" state (201 response code).
+
+    IMPORTANT: must only be used within BaseHTTPRequestHandler.
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        handler = args[0]  # self
+
+        try:
+            f(*args, **kwargs)
+        except Exception as e:
+            handler.send_response(401)
+            handler.send_header('Content-type','application/json')
+            handler.end_headers()
+
+            ret_json = {'error': str(e)}
+            ret_json = json.dumps(ret_json)
+            handler.wfile.write(ret_json.encode())
+
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            traceback.print_tb(exc_traceback, file=sys.stdout)
+
     return decorated_function
