4f2a5d9d0c8e096a577dd7c50021eb7ffb3c20ef
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-04-03 02:10:06 +0100

    feat: implement location and messages [WIP]
    Related tests now pass:
        * msg/location creation and retrieval
        * get msgs in range

--- locmess/locmess.py
+++ locmess/locmess.py
@@ -4,10 +4,14 @@ the directives to interact with the database, user managment, etc.
 """
 from pony.orm import *
 from locmess import settings
-from locmess.tables import User
+from locmess.settings import logging
+from locmess.tables import User, Message, Location
 import hashlib
+import json
 from cryptography.fernet import Fernet
 from locmess.decorators import authentication_required
+from haversine import haversine
+from datetime import datetime
 
 from base64 import b64encode, b64decode
 from locmess.exceptions import UserAlreadyExistsError
@@ -54,6 +58,103 @@ class LocMess(object):
         # NOTE: exception messages are useful for clients
         u = self.get_user(username)
         u.token = None
+
+    @authentication_required
+    def add_message(self, username, token, title, location, text,
+                    is_centralized=True, is_black_list=True,
+                    valid_from=None, valid_until=None,
+                    is_visible=True, properties=None):
+        """
+        Adds a new message.
+        """
+        if valid_from is None:
+            valid_from = datetime.now
+        if valid_until is None:
+            valid_until = datetime.max
+        if properties is None:
+            properties = json.dumps({})
+
+        author = None
+        with db_session:
+            author = self.get_user(username=username)
+
+        logging.debug('Adding message "{}" for user {}'.format(title, author))
+
+        msg = None
+        with db_session:
+            msg = Message(author=author, title=title, location=location,
+            text=text,
+            is_centralized=is_centralized, is_black_list=is_black_list,
+            valid_until=valid_until, properties=properties,
+            is_visible=is_visible)
+
+        return msg
+
+    @db_session
+    @authentication_required
+    def add_location(self, username, token, name, is_gps, location_json):
+        author = self.get_user(username)
+        logging.debug('Adding location "{}" is_gps: {},'
+                      'author: {}'.format(name, is_gps, author))
+        loc = Location(author=author, name=name, is_gps=is_gps, location=location_json)
+        return loc
+
+    @db_session
+    def get_location_by_name(self, name):
+        logging.debug('Getting location "{}" by name'.format(name))
+        return Location.get(name=name)
+
+    @db_session
+    @authentication_required
+    def get_available_messages_by_gps(self, username, token, curr_coord):
+        """
+        Gets a list of available messages for the user in the provided
+        location.
+
+        Args:
+            curr_coord - tuple of (latitude, longitude) of the current
+            user's location.
+        """
+        logging.debug('Request for available messages by GPS corrdinates')
+        msgs = self._get_messages_in_range(curr_coord)
+        return msgs
+
+    @db_session
+    def _get_messages_in_range(self, curr_coord):
+        gps_msgs = select(m for m in Message
+                            if m.location.is_gps)
+        gps_msgs_in_range = self._filter_msgs_in_range(gps_msgs, curr_coord)
+        return gps_msgs_in_range
+
+
+    def _filter_msgs_in_range(self, msgs, curr_coord):
+        res = [msg for msg in msgs
+                   if self._is_in_range_from_json_coord(
+                                    msg.location.location, curr_coord)
+              ]
+        return res
+
+    def _is_in_range_from_json_coord(self, json_coord, curr_coord):
+        loc_coord, radius = self._parse_location_from_json(json_coord)
+        logging.debug('Parsed location: '
+                      'latitude={}, longitude={}, radius={}'.format(
+                                    loc_coord[0], loc_coord[1], radius))
+        return self._is_in_range(loc_coord, curr_coord, radius)
+
+
+    def _parse_location_from_json(self, location_json):
+        location = json.loads(location_json)
+        latitude = location['latitude']
+        longitude = location['longitude']
+        radius = location['radius']
+
+        return ((latitude, longitude), radius)
+
+    def _is_in_range(self, ref_coord, my_coord, radius):
+        return self._get_distance_in_between_coordinates_in_meters(ref_coord, my_coord) <= radius
+
+    def _get_distance_in_between_coordinates_in_meters(self, coord1, coord2):
+        return haversine(coord1, coord2) * 1000
 
     def _get_password_hash(self, str_user, str_pwd):
         digest = hashlib.pbkdf2_hmac('sha256', str_pwd.encode(),
--- locmess/tables.py
+++ locmess/tables.py
@@ -1,5 +1,6 @@
 from pony.orm import *
 from locmess import settings
+from datetime import datetime
 
 db = Database()
 db.bind(settings.DB_TYPE, settings.DB_FILE_NAME, create_db=True)
@@ -8,6 +9,29 @@ class User(db.Entity):
     username = Required(str, unique=True)
     password = Required(str)
     token = Optional(bytes)
+    locations = Set('Location')
+    messages = Set('Message')
+
+class Location(db.Entity):
+    name = Required(str, unique=True)
+    author = Required(User)
+    is_gps = Required(bool)  # True: GPS; False: WiFi APs
+    location = Required(Json)
+    messages = Set('Message')
+
+
+class Message(db.Entity):
+    author = Required(User)
+    title = Required(str)
+    location = Required(Location)  # JSON with GPS coord or WiFi AP list
+    text = Required(str)
+    is_centralized = Required(bool)  # True: centralized; False: decentralized
+    is_black_list = Required(bool)  # True: blacklist; False: whitelist
+    valid_from = Required(datetime, default=datetime.now())
+    valid_until = Required(datetime, default=datetime.max)
+    time_posted = Required(datetime, default=datetime.now())
+    properties = Required(Json)
+    is_visible = Required(bool, default=True)
 
 
 db.generate_mapping(create_tables=True)
