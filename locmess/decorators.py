e00789d73e098dd77d1906bf7518f8c1c2593417
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-05-07 20:47:58 +0100

    feat: add error message after stack trace print

--- locmess/decorators.py
+++ locmess/decorators.py
@@ -70,5 +70,6 @@ def handle_expcetions(f):
 
             exc_type, exc_value, exc_traceback = sys.exc_info()
             traceback.print_tb(exc_traceback, file=sys.stdout)
+            print(str(e))
 
     return decorated_function
